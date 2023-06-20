5c9725d2a419bcb11eb7e43c38007ad9eac353a3
hadrienjeanne <hadrien.jeanne@free.fr>
2023-06-20 13:01:51 -0400

    modified settings for railway shipping

--- recipe/urls.py
+++ recipe/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name='recipe-home'),
     path('about/', views.about, name='recipe-about'),
 ]
-
+# allowing media serve in debug
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipeproject/settings.py
+++ recipeproject/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=6l19g@q9a(ppvesj&fxo8!o6cy)km4glgf&%1q2k$@5qr&ukf"
+# SECRET_KEY = "django-insecure-=6l19g@q9a(ppvesj&fxo8!o6cy)km4glgf&%1q2k$@5qr&ukf"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+                            "django-insecure-=6l19g@q9a(ppvesj&fxo8!o6cy)km4glgf&%1q2k$@5qr&ukf")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
