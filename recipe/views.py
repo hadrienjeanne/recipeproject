d754340a8e036526ea07c8470689f80eac21d7d3
hadrienjeanne <hadrien.jeanne@free.fr>
2023-06-21 09:47:55 -0400

    Added pagination

--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .recipe import Recipe
 
 def home(request):
+    recipes = Recipe.objects.all()
+    paginator = Paginator(recipes, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'recipes': Recipe.objects.all()
+        'recipes': page_obj
     }
     return render(request, 'recipe/home.html', context)
 
